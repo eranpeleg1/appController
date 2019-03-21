5b7b9f47bee473768f23e96a06bc01e266f40f88
eran peleg <32025731+eranpeleg1@users.noreply.github.com>
2019-03-21 16:11:05 +0200

    Update editorScript.js

--- editorScript.js
+++ editorScript.js
@@ -2,7 +2,7 @@ module.exports = {
     editorReady: async (editorSDK, token, {firstInstall}) => {
         if (firstInstall) {
             console.log('installing');
-            const componentRefs = await editorSDK.components.gelAllComponents()
+            const componentRefs = await editorSDK.components.getAllComponents()
             const result = await editorSDK.components.get(token, {componentRefs, properties: ['nickname']})
             console.log('result: ', result)
         }
